c139f8ec0ffeb8c4ae7bb2c019c46f6406297135
Zhazhazhu <fangyuan.leslie@qq.com>
2024-01-23 18:23:47 +0800

    fix: listen ip to 0.0.0.0

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> std::io::Result<()> {
             .service(web::scope("/api").service(clients).service(clients_by_ip))
     })
     .workers(2)
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
